ec9056d98fa8b9881f7b381a68575b5a8e613ad7
GenichiMaruo <85728541+GenichiMaruo@users.noreply.github.com>
2023-01-17 15:15:27 +0900

    スコア表示を追加

--- snakegame/snview.py
+++ snakegame/snview.py
@@ -15,22 +15,18 @@ class View:
         self.rectangle = Rectangle(Point(100, 100), Point(500, 500))
         self.rectangle.setFill("orange")
         self.rectangle.draw(self.window)
-        self.text = Text(Point(300, 650), "Snake Game")
+        self.text = Text(Point(300, 670), "Snake Game")
         self.text.setSize(36)
         self.text.draw(self.window)
+        self.score = Text(Point(300, 620), "Score: 0")
+        self.score.setSize(24)
+        self.score.draw(self.window)
         
     def draw(self, snake, food):
+        self.score.setText("Score: " + str(snake.getscore()))
         snake.draw(self.window)
         food.draw(self.window)
 
     def close(self):
         self.window.close()
     
-    def loop(self):
-        i = 0
-        while True:
-            i += 1
-            i = i % 6
-            
-            
-            break
